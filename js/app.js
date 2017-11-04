95ddd8c5d908431a9546bd76d34782c5bf5f8542
Ryan Hollinger <dispose99-ryan@yahoo.com>
2017-11-04 13:56:32 -0400

    Adds skeleton Bootstrap / Angular files

---
+++ js/app.js
@@ -0,0 +1,30 @@
+/**
+*  app.js
+* Skeleton Angular app.js
+*/
+
+// Create the main application module
+var app = angular.module("appname", ["ngRoute", "controller"]);
+
+// Set up routing for single-page application
+app.config(function($routeProvider, $locationProvider) {
+    // Added $locationProvider to solve "#!" routing issue
+    $locationProvider.hashPrefix('');
+    $routeProvider
+
+        .when("/", {
+            templateUrl: "views/mainview.html",
+            controller: "controller"
+        })
+/*
+        .when("/ANOTHERPAGE", {
+            templateUrl: "views/ANOTHERPAGE.html",
+            controller: "ANOTHERCTRL"
+        })
+*/
+        // Any other link
+        .otherwise({
+            redirectTo: "/"
+        });
+
+});
