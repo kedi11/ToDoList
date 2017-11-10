5415bc761a2ccc5bf0bc7ea3df048635f9c5a1ce
Ryan Hollinger <dispose99-ryan@yahoo.com>
2017-11-09 22:09:15 -0500

    Adds stub services for Users, Lists, and Tasks

--- js/controllers/controller.js
+++ js/controllers/controller.js
@@ -6,9 +6,21 @@
 angular.module("controller", [])
 
 // Create a (MVC) controller passing its name and array of dependencies starting with $scope
-.controller("controller", ["$scope", function($scope) {
+.controller("controller", ["$scope", "UsersService", "ListsService", "TasksService",
+    function($scope, UsersService, ListsService, TasksService) {
     // Always create an object first and add properties/methods to it instead of $scope
     $scope.object = {};
     $scope.object.skeleton = "Angular, Bootstrap, and JQuery Skeleton";
-}]);
-
+}])
+.service("UsersService", function(){
+    var usersService = {};
+    return usersService;
+})
+.service("ListsService", function(){
+    var listsService = {};
+    return listsService;
+})
+.service("TasksService", function(){
+    var tasksService = {};
+    return tasksService;
+});
