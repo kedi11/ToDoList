43ec34a5bb456f43bdc04b89af9373ab0c27fafd
Ryan Hollinger <dispose99-ryan@yahoo.com>
2017-11-09 23:02:09 -0500

    Adds array of dummy / test tasks to TasksService

--- js/controllers/controller.js
+++ js/controllers/controller.js
@@ -22,5 +22,47 @@ angular.module("controller", [])
 })
 .service("TasksService", function(){
     var tasksService = {};
+
+    tasksService.Tasks = [
+        // Dummy / test data
+        // Table fields: TaskID, ListID, sTaskName, bComplete, dDue, dCreated, dUpdated, dDeleted
+        {
+            taskID: 1001, listID: 100, taskName: "DUMMY1001", isComplete: false, 
+            dateDue: new Date("Dec 01, 2017"),
+            dateCreated: new Date("Nov 01, 2017"),
+            dateUpdated: new Date("Nov 01, 2017")
+        },
+        {
+            taskID: 1002, listID: 100, taskName: "DUMMY1002", isComplete: true, 
+            dateDue: new Date("Dec 02, 2017"),
+            dateCreated: new Date("Nov 02, 2017"),
+            dateUpdated: new Date("Nov 02, 2017")
+        },
+        {
+            taskID: 1003, listID: 100, taskName: "DUMMY1003", isComplete: false, 
+            dateDue: new Date("Dec 03, 2017"),
+            dateCreated: new Date("Nov 03, 2017"),
+            dateUpdated: new Date("Nov 03, 2017")
+        },
+        {
+            taskID: 1004, listID: 100, taskName: "DUMMY1004", isComplete: true, 
+            dateDue: new Date("Dec 04, 2017"),
+            dateCreated: new Date("Oct 01, 2017"),
+            dateUpdated: new Date("Oct 01, 2017")
+        },
+        {
+            taskID: 1005, listID: 100, taskName: "DUMMY1005", isComplete: false, 
+            dateDue: new Date("Dec 05, 2017"),
+            dateCreated: new Date("Oct 01, 2017"),
+            dateUpdated: new Date("Nov 01, 2017")
+        },
+        {
+            taskID: 1006, listID: 100, taskName: "DUMMY1006", isComplete: true, 
+            dateDue: new Date("2017-12-06T23:59:59"),
+            dateCreated: new Date("2017-11-01T12:00:00"),
+            dateUpdated: new Date("2017-11-01T18:00:00")
+        }
+    ];
+
     return tasksService;
 });
